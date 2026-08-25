aa85a2f3a0dc6aa8423479f32fbff3c5500dc6c9
Void <2624295165@qq.com>
2026-08-25 16:25:42 +0800

    feat: fill based body for Hare class

--- test/src/hare.cpp
+++ test/src/hare.cpp
@@ -1,2 +1,46 @@
-#include "hare.h"
+#include <hare.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/variant.hpp>
 
+Hare::Hare() {
+    hp = 0;
+    Fsm<Hare>::Handlers* handler= new Fsm<Hare>::Handlers(&handle_idle, &handle_wander);  
+}
+
+Hare::~Hare(){
+    delete fsm;
+    fsm = nullptr;
+}
+
+void Hare::set_hp(int64_t value){
+    hp = value;
+}
+
+int64_t Hare::get_hp() const {
+    return hp;
+}
+
+FSM_T::status_t handle_idle(FSM_T::event_t event){
+
+}
+
+FSM_T::status_t handle_wander(FSM_T::event_t event){
+
+}
+
+void Hare::_ready() {
+
+}
+
+void Hare::_physics_process(double p_delta) {
+
+}
+
+void Hare::_bind_methods(){
+    godot::ClassDB::bind_method(godot::D_METHOD("set_hp", "value"), &set_hp);
+    godot::ClassDB::bind_method(godot::D_METHOD("get_hp"), &get_hp);
+    godot::ClassDB::bind_method(godot::D_METHOD("handle_idle", "event"), &handle_idle);
+    godot::ClassDB::bind_method(godot::D_METHOD("handle_wander", "event"), &handle_wander);
+
+    ADD_PROPERTY(godot::PropertyInfo(godot::Variant::INT, "hp"), "set_hp", "get_hp");
+}
--- test/src/hare.h
+++ test/src/hare.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "enemy.h"
+#include "enemy.hpp"
+
+#define FSM_T Fsm<Hare>
 
 class Hare: public Enemy {
     GDCLASS(Hare, Enemy)
@@ -9,12 +11,17 @@ public:
     Hare();
     ~Hare();
 
-    void set_hp();
+    void set_hp(int64_t value);
     int64_t get_hp() const;
 
-    
+    void _ready() override;
+    void _physics_process(double p_delta) override;
+
+    FSM_T::status_t handle_idle(FSM_T::event_t event);
+    FSM_T::status_t handle_wander(FSM_T::event_t event);
 protected:
     static void _bind_methods();
 private:
     int64_t hp;
+    FSM_T* fsm;
 };
